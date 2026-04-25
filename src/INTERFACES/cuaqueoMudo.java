0f1b175b03edee2715f401f8ec3eee1979ffd605
Ximena Sanchez <sanchezximenavelasquez03@gmail.com>
2026-04-25 11:17:11 -0500

    creacion de pato

---
+++ src/INTERFACES/cuaqueoMudo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package INTERFACES;
+
+/**
+ *
+ * @author sistemas
+ */
+public interface cuaqueoMudo {
+    void cuauqear();
+    
+    
+}
---
+++ src/clases/Pato.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author sistemas
+ */
+public abstract class Pato {
+    public void nadar(){
+    System.out.println("TODOS LOS PATOS NADAN");
+}
+    public abstract void mostrar();
+}
+
